8d9fe113a97a00f820e3d11673abd943eabe5077
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-09-05 12:46:19 +0530

    added code for printing n to 1

--- counting.cpp
+++ counting.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+void counting(int n)
+{
+    if (n == 0)
+    {
+        return;
+    }
+    cout << n << endl;
+    counting(n - 1);
+}
+
 int main()
 {
+    counting(10);
+    // cout << counting(10) << endl;
 }
