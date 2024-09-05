1711bdd7c5a874842c5133507e11098c683d3da2
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-09-05 12:31:29 +0530

    factorial code by recursion

--- factorial.cpp
+++ factorial.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int factorial(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+
+    int recursionkaAns = n * factorial(n - 1);
+    return recursionkaAns;
+}
+
 int main()
 {
+    cout << factorial(5) << endl;
 }
