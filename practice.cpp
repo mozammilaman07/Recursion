7b4f281468024575e176663ed4733fd802606711
Md Mozammil Aman <amanmozammil.07@gmail.com>
2025-03-07 21:52:25 +0530

    factorial code

---
+++ practice.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n)
+{
+    // base case
+    if (n == 1)
+    {
+        return 1;
+    }
+    if (n == 0)
+    {
+        return 1;
+    }
+
+    // recursive call
+    int ans = n * factorial(n - 1);
+    return ans;
+}
+
+int main()
+{
+
+    int a = 5;
+    int finalAns = factorial(a);
+    cout << finalAns << endl;
+}
